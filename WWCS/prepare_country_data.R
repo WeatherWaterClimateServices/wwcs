67d9837dfb24c475c3477178d51914febd7d6b0c
borlowsky <44468024+borlowsky@users.noreply.github.com>
2024-11-15 12:13:16 +0100

    updated paths in prepare_country_data.R (WWCS instead of WWCS-MeteoSwiss)

--- WWCS/prepare_country_data.R
+++ WWCS/prepare_country_data.R
@@ -6,14 +6,14 @@
 #
 # ------------------------------------------------
 
-source('/opt/shiny-server/WWCS-MeteoSwiss/.Rprofile')
+source('/opt/shiny-server/WWCS/.Rprofile')
 library(raster)
 
-system(paste0('wget -P /opt/shiny-server/WWCS-MeteoSwiss/boundaries https://geodata.ucdavis.edu/gadm/gadm4.1/shp/gadm41_', gadm0 ,'_shp.zip'))
-system(paste0('unzip -o /opt/shiny-server/WWCS-MeteoSwiss/boundaries/gadm41_', gadm0 ,'_shp.zip -d /opt/shiny-server/WWCS-MeteoSwiss/boundaries/'))
+system(paste0('wget -P /opt/shiny-server/WWCS/boundaries https://geodata.ucdavis.edu/gadm/gadm4.1/shp/gadm41_', gadm0 ,'_shp.zip'))
+system(paste0('unzip -o /opt/shiny-server/WWCS/boundaries/gadm41_', gadm0 ,'_shp.zip -d /opt/shiny-server/WWCS/boundaries/'))
 
-country <- raster::shapefile(paste0('/opt/shiny-server/WWCS-MeteoSwiss/boundaries/gadm41_', gadm0, '_0.shp'))
+country <- raster::shapefile(paste0('/opt/shiny-server/WWCS/boundaries/gadm41_', gadm0, '_0.shp'))
 wld <- raster::rasterToPolygons(raster(ncol = 1, nrow = 1, crs = proj4string(country)))
 mask <- raster::erase(wld, country)
 mask_sf <- sf::st_as_sf(mask, crs = st_crs(4326))
-saveRDS(mask_sf, '/opt/shiny-server/WWCS-MeteoSwiss/boundaries/mask.rds')
+saveRDS(mask_sf, '/opt/shiny-server/WWCS/boundaries/mask.rds')
