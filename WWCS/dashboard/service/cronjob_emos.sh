25bab7e972502677e9b23e16c6e1c56c885763e0
obellprat <34236131+obellprat@users.noreply.github.com>
2024-11-25 16:23:28 +0100

    Update cronjob_emos.sh
    Ensure that the "cronout" folder exists

--- WWCS/dashboard/service/cronjob_emos.sh
+++ WWCS/dashboard/service/cronjob_emos.sh
@@ -9,6 +9,45 @@ echo "=== CRON JOB ==="
 
 cd /srv/shiny-server/dashboard/service
 
+
+#!/bin/bash
+# Exit immediately if any command exits with a non-zero status
+set -e
+
+# Exit immediately if a pipeline returns a non-zero status
+set -o pipefail
+
+echo "=== CRON JOB ==="
+
+cd /srv/shiny-server/dashboard/service
+
+# Ensure the cronout folder exists
+mkdir -p cronout
+
+echo "=== RETRIEVE EMCWF FORECASTS ==="
+
+./get_ecmwf/get_ecmwf.py > cronout/ecmwf.out
+
+./get_ecmwf/get_ecmwf_extended.py > cronout/ecmwf_extended.out
+
+./get_ecmwf/concatenate_ecmwf.py > cronout/ecmwf_concatenate.out
+
+echo "=== GET OBSERVATIONS AND POSTPROCESS FORECASTS ==="
+
+R CMD BATCH --no-save forecasts/EMOS.R cronout/emos.out
+
+echo "=== PROCESS WEATHER PICTOGRAMS FROM FORECASTS ==="
+
+R CMD BATCH --no-save forecasts/process_pictos.R  cronout/pictos.out
+
+R CMD BATCH --no-save forecasts/prepare_api_data.R  cronout/api.out
+
+
+
+
+
+
+
 echo "=== RETRIEVE EMCWF FORECASTS ==="
 
 ./get_ecmwf/get_ecmwf.py > cronout/ecmwf.out
