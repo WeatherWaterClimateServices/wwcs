e374bf1030f0bfefbab89e3413651691b3b5d952
J. David Ibáñez <jdavid.ibp@gmail.com>
2025-06-24 15:55:16 +0200

    Alembic database url

--- API/alembic/env.py
+++ API/alembic/env.py
@@ -1,9 +1,16 @@
 from logging.config import fileConfig
+import os
+import pathlib
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+import dotenv
 
 from alembic import context
+
+# Load environment variables from .env in the parent directory
+env_path = pathlib.Path(__file__).resolve().parent.parent / '.env'
+dotenv.load_dotenv(env_path)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -57,8 +64,9 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    db_url = os.getenv('DB_URL')
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        {'sqlalchemy.url': db_url},
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
