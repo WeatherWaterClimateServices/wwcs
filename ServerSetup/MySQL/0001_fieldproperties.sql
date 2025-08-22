b3b1ac6a69cdde44a59d08f1dacda29c6947def1
Boris Orlowsky <borlowsky@caritas.ch>
2025-08-22 13:32:56 +0200

    set defaults for the warnlevels and fieldproperties columns of SitesHumans.Sites to NULL

--- ServerSetup/MySQL/0001_fieldproperties.sql
+++ ServerSetup/MySQL/0001_fieldproperties.sql
@@ -1,16 +1,7 @@
 ALTER TABLE SitesHumans.Sites
-ALTER COLUMN fieldproperties
-SET DEFAULT JSON_OBJECT(
-  'StartDate', '2023-06-1',
-  'FC', 38,
-  'WP', 18,
-  'WA', 0.5,
-  'IE', 0.65,
-  'MAD', 0.4,
-  'PHIc', 22,
-  'Crop', 'Potato',
-  'area', 0,
-  'type', 'channel',
-  'humanID', 10001
-);
+ALTER COLUMN warnlevels
+SET DEFAULT NULL;
 
+ALTER TABLE SitesHumans.Sites
+ALTER COLUMN fieldproperties
+SET DEFAULT NULL;
