ee7c358a069900099cabc45c507f992ec6155b65
borlowsky <borlowsky@caritas.ch>
2026-09-07 19:57:16 +0500

    cronjob-OM: download mode only

--- WWCS/dashboard/service/cronjob_OM.sh
+++ WWCS/dashboard/service/cronjob_OM.sh
@@ -20,7 +20,7 @@ echo "=== RETRIEVE OPEN-METEO (ECMWF) FORECASTS ==="
 # use the venv in  venv - this will usually be ~/venv, but may not in dev setups
 ../../../../venv/bin/python3 get_open_meteo/get_open_meteo.py > cronout/open-meteo.out
 
-#../../../../venv/bin/python3 get_open_meteo/get_open_meteo_grid.py > cronout/open-meteo-grd.out
+../../../../venv/bin/python3 get_open_meteo/get_open_meteo_grid.py > cronout/open-meteo-grd.out
 
 ../../../../venv/bin/python3 get_open_meteo/concatenate_open-meteo.py > cronout/open-meteo-concat.out
 
@@ -29,10 +29,10 @@ echo "=== GET OBSERVATIONS AND POSTPROCESS FORECASTS ==="
 cd ../..
 pwd
 
-R CMD BATCH --no-save ${my_dir}/forecasts/EMOS.R ${my_dir}/cronout/emos.out
+#R CMD BATCH --no-save ${my_dir}/forecasts/EMOS.R ${my_dir}/cronout/emos.out
 
 echo "=== PROCESS WEATHER PICTOGRAMS FROM FORECASTS ==="
 
-R CMD BATCH --no-save ${my_dir}/forecasts/process_pictos.R  ${my_dir}/cronout/pictos.out
+#R CMD BATCH --no-save ${my_dir}/forecasts/process_pictos.R  ${my_dir}/cronout/pictos.out
 
-R CMD BATCH --no-save ${my_dir}/forecasts/prepare_api_data.R  ${my_dir}/cronout/api.out
+#R CMD BATCH --no-save ${my_dir}/forecasts/prepare_api_data.R  ${my_dir}/cronout/api.out
